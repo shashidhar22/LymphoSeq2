d607f980949f3e2d88669b103da2e7f91e1b205d
Shashidhar Ravishankar <shashidhar.r.shankar@gmail.com>
2020-08-12 19:31:33 -0700

    Updated aling to remove redundancy, initial test pass, unit test pending

--- R/alignSeq.R
+++ R/alignSeq.R
@@ -1,4 +1,4 @@
-#' Align mutliple sequences
+#' Align multiple sequences
 #' 
 #' Perform multiple sequence alignment using one of three methods and output results to the 
 #' console or as a pdf file.  One may perform the alignment of all amino acid or junction
@@ -9,16 +9,16 @@
 #' imported by the LymphoSeq function readImmunoSeq.
 #' @param repertoire_id A character vector indicating the name of the repertoire_id in the productive
 #' sequence list. 
-#' @param sequence A character vector of one ore more amino acid or junction 
+#' @param sequence_list A character vector of one ore more amino acid or junction 
 #' CDR3 sequences to search.
-#' @param editDistance An integer giving the minimum edit distance that the 
+#' @param edit_distance An integer giving the minimum edit distance that the 
 #' sequence must be less than or equal to.  See details below.
 #' @param type A character vector indicating whether "junction_aa" or "junction" sequences
 #' should be aligned.  If "junction_aa" is specified, then run productiveSeqs first.
 #' @param method A character vector indicating the multiple sequence alignment method to 
-#' be used.  Refer to the Bioconductor msa package for more details.  Options incude 
+#' be used.  Refer to the Bioconductor msa package for more details.  Options include 
 #' "ClustalW", "ClustalOmega", and "Muscle".
-#' @param output A character vector indicating where the multiple sequence alignemnt should be
+#' @param output A character vector indicating where the multiple sequence alignment should be
 #' printed.  Options include "console" or "pdf".  If "pdf" is selected, the file is saved to
 #' the working directory.  For "pdf" to work, Texshade must be installed.  Refer to the 
 #' Bioconductor package msa installation instructions for more details.
@@ -28,9 +28,9 @@
 #' means the sequences are identical and an edit distance of 1 indicates that 
 #' the sequences different by a single amino acid or junction.
 #' 
-#' @return Performs a multiple sequence alignemnt and prints to the console or saves a pdf to 
+#' @return Performs a multiple sequence alignment and prints to the console or saves a pdf to 
 #' the working directory.
-#' @seealso If having trouble saving pdf files, refer to Biconductor package msa for
+#' @seealso If having trouble saving pdf files, refer to Bioconductor package msa for
 #' installation instructions
 #' \url{http://bioconductor.org/packages/release/bioc/vignettes/msa/inst/doc/msa.pdf}
 #' @examples
@@ -47,33 +47,51 @@
 #' @importFrom Biostrings AAStringSet
 #' @import msa
 #' @import tidyverse
-alignSeq = function(study_table, sample_list = NULL, sequence_list = NULL, editDistance = 15, output = "console", type = "junction", method = "ClustalOmega") {
-    if(!is.null(sequence_list) & is.null(sample_list)){
-        search_table <- searchSeq(study_table = study_table, sequence = sequence_list, editDistance = editDistance, type = type, match = "partial")
+alignSeq = function(study_table, repertoire_ids = NULL, 
+                    sequence_list = NULL, edit_distance = 15, 
+                    output = "console", type = "junction", 
+                    method = "ClustalOmega", top = 150) {
+    if(!is.null(sequence_list) & is.null(repertoire_ids)){
+        search_table <- LymphoSeq::searchSeq(study_table = study_table, 
+                                             sequence = sequence_list, 
+                                             edit_distance = edit_distance, 
+                                             type = type, 
+                                             match = "partial")
         if(is.null(search_table)){
             stop("There are no sequences to be aligned", call. = FALSE)
         }
     }
-    if(!is.null(sequence_list) & !is.null(sample_list)){
-        study_table %>% study_table %>% filter(repertoire_id %in% sample_list)
-        search_table <- searchSeq(list = list[repertoire_id], sequence = sequence_list, editDistance = editDistance, type = type, match = "partial")
+    if(!is.null(sequence_list) & !is.null(repertoire_ids)){
+        study_table <- study_table %>% 
+                       dplyr::filter(repertoire_id %in% repertoire_ids)
+        search_table <- LymphoSeq2::searchSeq(study_table = study_table,
+                                              sequence = sequence_list, 
+                                              edit_distance =  edit_distance, 
+                                              type = type, 
+                                              match = "partial")
         if(is.null(search_table)){
             stop("There are no sequences to be aligned", call. = FALSE)
         }
     }
     if(is.null(sequence_list)){
-        search_table <- study_table %>% filter(repertoire_id %in% sample_list)
+        search_table <- study_table %>% 
+                        dplyr::filter(repertoire_id %in% repertoire_ids)
     }
-    if(nrow(search_table) > 150){
+    if(nrow(search_table) > top){
         if(is.null(sequence_list)){
-            search_table <- search_table %>% top_n(150) 
+            search_table <- search_table %>% 
+                            dplyr::top_n(top) 
         } else {
-            search_table <- search_table %>% group_by(searchSequence) %>% top_n(150) %>% ungroup()
+            search_table <- search_table %>% 
+                            dplyr::group_by(searchSequence) %>% 
+                            dplyr::top_n(150) %>% 
+                            dplyr::ungroup()
         }
         message("Only 150 sequences sampled equally from each search group will be selected")
     }
     if(type == "junction"){
-        search_table <- search_table %>% filter(nchar(junction) > 15)
+        search_table <- search_table %>% 
+                        dplyr::filter(nchar(junction) > 15)
         string_list <- Biostrings::DNAStringSet(search_table$junction)
     }
     if(type == "junction_aa"){
