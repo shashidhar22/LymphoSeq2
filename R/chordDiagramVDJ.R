a5cd284bcb884143466e8028bae4817f18b3586c
Shashidhar Ravishankar <shashidhar.r.shankar@gmail.com>
2020-08-20 02:17:43 -0700

    Chord diagram code updated to follow tidyverse syntax. Tibble directly passed to circlize rather over matrix conversion

--- R/chordDiagramVDJ.R
+++ R/chordDiagramVDJ.R
@@ -3,7 +3,7 @@
 #' Creates a chord diagram showing VJ or DJ gene associations from one or more 
 #' samples.
 #' 
-#' @param repertoire_id A tibble consisting of frequencies of antigen receptor 
+#' @param study_table A tibble consisting of frequencies of antigen receptor 
 #' sequences.  "v_family", "j_family", and if applicable, "d_family" 
 #' are a required columns.  Using output from the LymphoSeq function topSeqs is
 #' recommended.
@@ -34,43 +34,53 @@
 #' @import tidyverse
 chordDiagramVDJ <- function(study_table, association = "VJ", colors = c("red", "blue")) {
     if (association == "VJ") {
-        if (!all(c("v_family", "j_family") %in% colnames(study_table))) {
+        if (!base::all(c("v_family", "j_family") %in% base::colnames(study_table))) {
             stop("The source data frame does not contain the required columns 'v_family' and 'j_family'.")
         }
         vj <- study_table %>% 
-              select(v_family, j_family) %>% 
-              mutate(v_family = replace_na(v_family, "Unresolved"), j_family = replace_na(j_family, "Unresolved"))
+              dplyr::select(v_family, j_family) %>% 
+              dplyr::mutate(v_family = tidyr::replace_na(v_family, "unresolved"), 
+                            j_family = tidyr::replace_na(j_family, "unresolved"))
         vj <- vj %>% 
-              group_by(v_family, j_family) %>% 
-              summarize(duplicate_count = n()) %>% 
-              pivot_wider(id_cols=v_family, names_from = j_family, values_from = duplicate_count) 
-        row_names <- vj$v_family 
-        vj <- vj %>% select(-v_family) %>% as.matrix()
-        rownames(vj) <- row_names
-        ribbon.color <- circlize::colorRamp2(range(vj), c("grey", "black"))
+              dplyr::group_by(v_family, j_family) %>% 
+              dplyr::summarize(duplicate_count = dplyr::n()) %>% 
+              dplyr::mutate(duplicate_count = tidyr::replace_na(duplicate_count, 0)) %>%
+              dplyr::rename(from = v_family,
+                            to = j_family,
+                            value = duplicate_count) %>%
+              dplyr::ungroup() %>%
+              dplyr::mutate(value = as.integer(value))
+        vcolors <- base::rep(colors[1], base::length(base::unique(vj$from)))
+        jcolors <- base::rep(colors[2], base::length(base::unique(vj$to)))
+        print(vj)
+        ribbon.color <- circlize::colorRamp2(range(vj$value), c("grey", "black"))
         circlize::chordDiagram(vj, 
                                annotationTrack = c("grid", "name"), 
-                               grid.col = c(rep(colors[1], dim(vj)[1]), rep(colors[2], dim(vj)[2])), 
+                               grid.col = c(vcolors, jcolors), 
                                col = ribbon.color)
     }
     if (association == "DJ") {
-        if (!all(c("d_family", "j_family") %in% colnames(study_table))) {
+        if (!base::all(c("d_family", "j_family") %in% base::colnames(study_table))) {
             stop("The source data frame does not contain the required columns 'd_family' and 'j_family'.")
         }
         dj <- study_table %>% 
-              select(d_family, j_family) %>% 
-              mutate(d_family = replace_na(d_family, "Unresolved"), j_family = replace_na(j_family, "Unresolved"))
+              dplyr::select(d_family, j_family) %>% 
+              dplyr::mutate(d_family = tidyr::replace_na(d_family, "Unresolved"), 
+                            j_family = tidyr::replace_na(j_family, "Unresolved"))
         dj <- dj %>% 
-              group_by(d_family, j_family) %>% 
-              summarize(duplicate_count = n()) %>% 
-              pivot_wider(id_cols=d_family, names_from = j_family, values_from = duplicate_count)
-        row_names <- dj$d_family 
-        dj <- dj %>% select(-d_family) %>% as.matrix()
-        rownames(dj) <- row_names
-        ribbon.color <- circlize::colorRamp2(range(dj), c("grey", "black"))
+              dplyr::group_by(d_family, j_family) %>% 
+              dplyr::summarize(duplicate_count = dplyr::n()) %>% 
+              dplyr::mutate(duplicate_count = tidyr::replace_na(duplicate_count, 0)) %>%
+              dplyr::mutate(duplicate_count = tidyr::replace_na(duplicate_count, 0)) %>%
+              dplyr::rename(from = d_family,
+                            to = j_family,
+                            value = duplicate_count)
+        dcolors <- base::rep(colors[1], base::length(base::unique(dj$from)))
+        jcolors <- base::rep(colors[2], base::length(base::unique(dj$to)))
+        ribbon.color <- circlize::colorRamp2(range(dj$value), c("grey", "black"))
         circlize::chordDiagram(dj, 
                                annotationTrack = c("grid", "name"), 
-                               grid.col = c(rep(colors[1], dim(dj)[1]), rep(colors[2], dim(dj)[2])), 
+                               grid.col = c(dcolors, jcolors), 
                                col = ribbon.color)
     }
 }
