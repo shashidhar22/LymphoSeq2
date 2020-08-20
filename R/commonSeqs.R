c1ea57fe58e314be52807f40ba14dc9f37298d06
Shashidhar Ravishankar <shashidhar.r.shankar@gmail.com>
2020-08-20 02:23:28 -0700

    commonSeq now uses cloneTrack to identify common sequences found across multiple repertoires in the study tibble

--- R/commonSeqs.R
+++ R/commonSeqs.R
@@ -3,10 +3,10 @@
 #' Creates a data frame of the common sequences in two or more repertoire_ids, reporting 
 #' their frequencies in each.
 #' 
+#' @param study_table A list of productive amino acid sequences generated
+#' by the LymphoSeq function productiveSeq where aggregate = "junction_aa".
 #' @param repertoire_ids A character vector of two or more repertoire_id names in 
 #' productive.aa.
-#' @param productive_aa A list of productive amino acid sequences generated
-#' by the LymphoSeq function productiveSeq where aggregate = "junction_aa".
 #' @return Returns a data frame of the common sequences between two or more files 
 #' displaying their frequencies in each.
 #' @seealso \code{\link{commonSeqsVenn}}
@@ -20,18 +20,15 @@
 #' commonSeqs(repertoire_ids = c("TRB_Unsorted_0", "TRB_Unsorted_32"), 
 #'    productive_aa = productive_aa)
 #' @export
-#' @importFrom plyr llply
-#' @import ggplot2
-#' @import tidyverse
-commonSeqs <- function(repertoire_ids, productive_aa) {
-    
-    common_seqs <- productive_aa %>% 
-                   filter(repertoire_id %in% repertoire_ids) %>%
-                   add_column(seen = 1) %>%
-                   pivot_wider(id_cols=junction_aa, 
-                               names_from=repertoire_id, 
-                               values_from=duplicate_count, 
-                               values_fill=list(duplicate_count=0)) %>%
-                   filter_at(vars(-junction_aa), all_vars(. != 0)) 
+commonSeqs <- function(study_table, repertoire_ids = NULL) {
+    if (base::is.null(repertoire_ids)) {
+      repertoire_ids <- study_table %>%
+                        dplyr::pull(repertoire_id) %>%
+                        base::unique()
+    }
+    common_seqs <- study_table %>% 
+                   LymphoSeq2::cloneTrack(sample_list = repertoire_ids) %>%
+                   dplyr::filter(seen > 1) %>%
+                   LymphoSeq2::seqMatrix()
     return(common_seqs)
 }
