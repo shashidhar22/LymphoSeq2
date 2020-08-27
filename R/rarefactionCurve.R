24e23fac500e9af602330f4f24fe821aae7689df
Shashidhar Ravishankar <shashidhar.r.shankar@gmail.com>
2020-08-26 20:20:21 -0700

    Progress bar removed

--- R/rarefactionCurve.R
+++ R/rarefactionCurve.R
@@ -18,21 +18,17 @@
 #' rarefaction_curve <- rarefactionCurve(study_table)
 #'
 #' @export
-#' @import tidyverse
-#' @import purrr
-
 plotRarefactionCurve <- function(study_table) {
-    progress <- dplyr::progress_estimated(length(unique(study_table$sample)))
     rarefaction_tables <- study_table %>% 
                           dplyr::group_by(repertoire_id) %>% 
                           dplyr::group_split() %>% 
-                          dplyr::map(~runINext(.x, progress)) %>% 
+                          purrr::map(runINext) %>% 
                           dplyr::bind_rows()
     rarefaction_tables <- rarefaction_tables %>% 
                           dplyr::mutate(method = recode(method, observed = "Interpolated", 
                                         interpolated = "Interpolated", extrapolated = "Extrapolated")) 
-    rarefaction_curves <- ggplot2::ggplot(rarefaction_tables, aes(x=m, y=qD, fill=sample)) + 
-                          ggplot2::geom_line(aes(linetype=method, color=sample), size=1.5) +  
+    rarefaction_curves <- ggplot2::ggplot(rarefaction_tables, aes(x=m, y=qD, fill=repertoire_id)) + 
+                          ggplot2::geom_line(aes(linetype=method, color=repertoire_id), size=1.5) +  
                           ggplot2::geom_ribbon(aes(ymin = qD.LCL, ymax = qD.UCL), alpha=0.5) +
                           ggplot2::scale_linetype_manual(values=c("dashed", "solid"),  
                                                          labels=c("Extrapolated", "Intrapolated")) + 
