314b95aafca581883a28ee82ed1ddcd43bbd40b0
elulu3 <elena.wu2000@gmail.com>
2022-05-09 11:52:48 -0700

    code syntax

--- R/readImmunoSeq.R
+++ R/readImmunoSeq.R
@@ -195,6 +195,49 @@ iReceptorFormat <- function(clone_frame) {
     return(iReceptor_frame)
 }
 
+
+mostPrevalent <- function(barcode_data) {
+    ab_chains <- barcode_data %>%
+                    dplyr::pull(v_call)
+    if (length(ab_chains) > 2) {
+        vdj_TRA <- barcode_data %>%
+                    dplyr::filter(grepl("TRA", v_call)) %>%
+                    dplyr::select(c(v_call, j_call, d_call))
+        if (nrow(vdj_TRA) > 1) {
+            TRA_count <- purrr::map(1:nrow(vdj_TRA),
+                                        function(x)
+                                            clone_data %>%
+                                                dplyr::filter(v_call == vdj_TRA$v_call[x],
+                                                                d_call == vdj_TRA$d_call[x] | is.na(d_call),
+                                                                j_call == vdj_TRA$j_call[x]) %>%
+                                                nrow())
+            barcode_data <- barcode_data %>%
+                                dplyr::filter(grepl("TRB", v_call) |
+                                                v_call == vdj_TRA$v_call[which.max(TRA_count)])
+
+        }
+        vdj_TRB <- barcode_data %>%
+                    dplyr::filter(grepl("TRB", v_call)) %>%
+                    dplyr::select(c(v_call, j_call, d_call))
+        if (nrow(vdj_TRB) > 1) {
+            TRB_count <- purrr::map(1:nrow(vdj_TRB),
+                        function(x)
+                            clone_data %>%
+                                dplyr::filter(v_call == vdj_TRB$v_call[x],
+                                                d_call == vdj_TRB$d_call[x] | is.na(d_call),
+                                                j_call == vdj_TRB$j_call[x]) %>%
+                                nrow())
+            barcode_data <- barcode_data %>%
+                                dplyr::filter(grepl("TRA", v_call) |
+                                                v_call == vdj_TRB$v_call[which.max(TRB_count)])
+        }
+    }
+    barcode_data <- barcode_data %>%
+                        dplyr::mutate_all(~paste(., collapse = ".")) %>%
+                        head(1)
+    return(barcode_data)
+}
+
 read10x <- function(clone_data) {
     # double_fields <- c(v_sequence_start, v_sequence_end,
     #                     d_sequence_start, d_sequence_end,
@@ -202,48 +245,6 @@ read10x <- function(clone_data) {
     #                     c_sequence_start, c_sequence_end,
     #                     junction_length, junction_aa_length,
     #                     consensus_count, duplicate_count)
-
-    mostPrevalent <- function(barcode_data) {
-        ab_chains <- barcode_data %>%
-                        dplyr::pull(v_call)
-        if (length(ab_chains) > 2) {
-            vdj_TRA <- barcode_data %>%
-                        dplyr::filter(grepl("TRA", v_call)) %>%
-                        dplyr::select(c(v_call, j_call, d_call))
-            if (nrow(vdj_TRA) > 1) {
-                TRA_count <- purrr::map(1:nrow(vdj_TRA),
-                                            function(x)
-                                                clone_data %>%
-                                                    dplyr::filter(v_call == vdj_TRA$v_call[x],
-                                                                  d_call == vdj_TRA$d_call[x] | is.na(d_call),
-                                                                  j_call == vdj_TRA$j_call[x]) %>%
-                                                    nrow())
-                barcode_data <- barcode_data %>%
-                                    dplyr::filter(grepl("TRB", v_call) |
-                                                  v_call == vdj_TRA$v_call[which.max(TRA_count)])
-
-            }
-            vdj_TRB <- barcode_data %>%
-                        dplyr::filter(grepl("TRB", v_call)) %>%
-                        dplyr::select(c(v_call, j_call, d_call))
-            if (nrow(vdj_TRB) > 1) {
-                TRB_count <- purrr::map(1:nrow(vdj_TRB),
-                            function(x)
-                                clone_data %>%
-                                    dplyr::filter(v_call == vdj_TRB$v_call[x],
-                                                    d_call == vdj_TRB$d_call[x] | is.na(d_call),
-                                                    j_call == vdj_TRB$j_call[x]) %>%
-                                    nrow())
-                barcode_data <- barcode_data %>%
-                                    dplyr::filter(grepl("TRA", v_call) |
-                                                  v_call == vdj_TRB$v_call[which.max(TRB_count)])
-            }
-        }
-        barcode_data <- barcode_data %>%
-                            dplyr::mutate_all(~paste(., collapse = ".")) %>%
-                            head(1)
-        return(barcode_data)
-    }
     double_fields <- c("v_sequence_start", "v_sequence_end",
                 "d_sequence_start", "d_sequence_end",
                 "j_sequence_start", "j_sequence_end",
